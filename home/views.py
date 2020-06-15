b4d4bbe3a91f115bd7f3d40e7b5a4307a6f8341d
jhbrvfjhbr <64678614+jhbrvfjhbr@users.noreply.github.com>
2020-06-15 10:44:52 +0300

    Site
    new site

--- home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-	return render(request, 'home.html')
-def pudge(request):
-	return render(request, 'pudge.html')
-def antimage(request):
-	return render(request, 'antimage.html')
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blabla.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medic.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
